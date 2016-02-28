35c910c04f547fec60c2e95258a2f7240ff665ae
Michael Eden <themichaeleden@gmail.com>
2016-02-27 22:08:01 -0500

    can get current brightness

--- src/manage/brightness.rs
+++ src/manage/brightness.rs
@@ -17,6 +17,17 @@ pub fn get_max_brightness() -> Result<f64, Error> {
     Ok(max_brightness)
 }
 
+pub fn get_brightness() -> Result<f64, Error> {
+    let mut buffer = String::new();
+    let max_path = Path::new(BRIGHTNESS_DIR).join("brightness");
+
+    let mut max_brightness = try!(File::open(max_path));
+    max_brightness.read_to_string(&mut buffer);
+    let max_brightness: f64 = try!(buffer.trim().parse());
+
+    Ok(max_brightness)
+}
+
 impl From<IoError> for Error {
     #[inline]
     fn from(err: IoError) -> Self {
