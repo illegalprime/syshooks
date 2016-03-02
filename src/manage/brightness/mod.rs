b8ebf75163e56ef94a87dd28b11f329053aa724c
Michael Eden <themichaeleden@gmail.com>
2016-03-02 00:12:13 -0500

    added general method to change brightness

--- src/manage/brightness/mod.rs
+++ src/manage/brightness/mod.rs
@@ -10,4 +10,19 @@ pub trait Brightness {
     fn min(&self) -> Result<f64, Self::E>;
     fn current(&self) -> Result<f64, Self::E>;
     fn set(&self, value: f64) -> Result<(), Self::E>;
+
+    fn change_n_clip(&self, delta: f64) -> Result<(), Self::E> {
+        let (min, max) = (try!(self.min()), try!(self.max()));
+        let current = try!(self.current());
+
+        let next = if current + delta > max {
+            max
+        } else if current + delta < min {
+            min
+        } else {
+            current + delta
+        };
+
+        self.set(next)
+    }
 }
